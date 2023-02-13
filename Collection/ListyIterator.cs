9d002729285510572a10e0725af4ead5a40b88dd
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-13 16:31:13 +0200

    2

---
+++ Collection/ListyIterator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    internal class ListyIterator<T> : IEnumerable<T>
+    {
+        private int index = 0;
+        private List<T> items;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+        }
+
+        public bool Move()
+        {
+            if (index < items.Count - 1)
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < items.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine($"{this.items[index]}");
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in this.items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Collection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collection
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<string> items = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList();
+
+            ListyIterator<string> listyIterator = new ListyIterator<string>(items);
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Print")
+                {
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (InvalidOperationException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+                else if (command == "PrintAll")
+                {
+                    Console.WriteLine(string.Join(" ", listyIterator));
+                }
+            }
+        }
+    }
+}
