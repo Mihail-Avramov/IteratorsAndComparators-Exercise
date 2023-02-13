216acf0f24251184d661620c5c6b7c3a305444dc
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-13 17:20:57 +0200

    4

---
+++ Stack/CustomStack.cs
@@ -0,0 +1,73 @@
+﻿
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private const int initialCapacity = 4;
+        private T[] items;
+
+        public CustomStack()
+        {
+            this.items = new T[initialCapacity];
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            if (this.Count == this.items.Length)
+            {
+                Resize();
+            }
+
+            this.items[Count] = item;
+
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            T removedItem = this.items[Count - 1];
+
+            this.Count--;
+            
+            return removedItem;
+        }
+
+        private void Resize()
+        {
+            T[] copy = new T[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return this.items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            CustomStack<int> customStack = new CustomStack<int>();
+
+            string input= string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inputArguments = input.Split(" ",2, StringSplitOptions.RemoveEmptyEntries);
+
+                string command = inputArguments[0];
+
+                if (command == "Push")
+                {
+                    if (inputArguments.Length > 1)
+                    {
+                        int[] itemsToPush = inputArguments[1].Split(", ").Select(int.Parse).ToArray();
+
+                        foreach (var item in itemsToPush)
+                        {
+                            customStack.Push(item);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input!");
+                    }
+                    
+                }
+                else if (command == "Pop")
+                {
+                    try
+                    {
+                        customStack.Pop();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
