aea4dd4c4b9b66a920e78cde3ef683e93bd01aa1
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-13 16:01:58 +0200

    Add project files.

---
+++ ListyIterator/ListyIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    internal class ListyIterator<T>
+    {
+        private int index = 0;
+        private List<T> items;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+        }
+
+        public bool Move()
+        {
+            if (index < items.Count - 1)
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < items.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine($"{this.items[index]}");
+        }
+
+    }
+}
---
+++ ListyIterator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<string> items = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList();
+
+            ListyIterator<string> listyIterator = new ListyIterator<string>(items);
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Print")
+                {
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (InvalidOperationException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+            }
+
+        }
+    }
+}
