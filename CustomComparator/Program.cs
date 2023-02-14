765d5006f79e2e6c13757fbde042fc0771d6d3db
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-14 15:37:05 +0200

    8

---
+++ CustomComparator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+Func<int, int, int> customComparator = (num1, num2) =>
+{
+    if (num1 % 2 == 0 && num2 % 2 != 0)
+    {
+        return -1;
+    }
+
+    if (num1 % 2 != 0 && num2 % 2 == 0)
+    {
+        return 1;
+    }
+
+    return num1.CompareTo(num2);
+};
+
+int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+
+
+Array.Sort(numbers, (num1, num2) => customComparator(num1, num2));
+
+Console.WriteLine(string.Join(" ", numbers));
